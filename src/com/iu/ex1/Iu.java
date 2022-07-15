58623cf4e95399afea1304a305067f618208d110
hyeongminyoo <dbgudals2004@gmail.com>
2022-07-15 10:15:35 +0900

    0715 10:15

---
+++ src/com/iu/ex1/Iu.java
@@ -0,0 +1,45 @@
+package com.iu.ex1;
+
+public class Iu {
+
+	private String name;
+	private int age;
+	private static Iu iu = null; //클래스 메서드 내에서 사용 가능
+	
+	//Setter
+	//public void set변수명(데이터타입 변수명){}
+	
+	//Getter
+	//public return타입 get변수명(){}
+	
+	
+	//기본생성자
+	private Iu() {}
+	
+	
+	//클래스 메서드가 됨 ( 객체 생성하지않고 사용) 
+	//없으면 새로 있으면 있는주소 리턴
+	public static Iu getInstance() {
+		
+		if(Iu.iu == null) {
+			Iu.iu = new Iu();
+		}
+		return Iu.iu;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
